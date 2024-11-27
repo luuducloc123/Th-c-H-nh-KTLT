e8dd92d45ec988a095b502e8a06bd91457bf5164
luuducloc123 <kienk12345679@gmail.com>
2024-11-27 09:29:58 +0700

    B7_Câu1.py

--- Bài_TH7/B7_Câu1.py
+++ Bài_TH7/B7_Câu1.py
@@ -1,1 +1,12 @@
-
+print ( "SV: Luu Duc Loc , msv: 205751030110045 ")
+print (" ###############")
+#################
+input_file =open('D:/a.txt','r')
+for line in input_file:
+    l=len(line)
+    S=''
+    while l>=1:
+        s=S+line [l-1]
+        l=l-1
+    print(s)
+input_file.close()
