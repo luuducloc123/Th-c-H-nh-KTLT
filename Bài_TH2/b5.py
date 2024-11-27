9064b9487935b7582e704c095cbdb3a1436671cb
luuducloc123 <kienk12345679@gmail.com>
2024-11-27 09:03:34 +0700

    Add files via upload

---
+++ Bài_TH2/b5.py
@@ -0,0 +1,7 @@
+print ( "SV: Luu Duc Loc , msv: 205751030110045 ")
+print (" ###############")
+#################
+n=int (input("enter a number---->"))
+while n>0:
+    print (n);
+    n=n-1;
---
+++ Bài_TH2/b7.py
@@ -0,0 +1,8 @@
+print ( "SV: Luu Duc Loc , msv: 205751030110045 ")
+print (" ###############")
+#################
+n=int(input("Nhập vào một số:"))
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+print (d)
