f942c0c5d982bd962d1c5279809d33b64f8f6617
luuducloc123 <kienk12345679@gmail.com>
2024-11-27 09:11:50 +0700

    B3_Câu1.py

--- Bài_TH3/B3_Câu1.py
+++ Bài_TH3/B3_Câu1.py
@@ -1,1 +1,9 @@
-
+print ( " sv: Luu Duc Loc ; msv : 205751030110045")
+print (" ############################# ")
+###################
+def sum(a,b):
+    print("sum="+str(a+b))
+# tinh tong 2 so 4,5
+sum(4,5)
+#tinh tong 2 so 3,7
+sum(3,7)
