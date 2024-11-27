e3e39709ac7305f1306d1ed6d481437a9ad7293a
luuducloc123 <kienk12345679@gmail.com>
2024-11-27 08:57:22 +0700

    B2_Câu1.py

--- Bài_TH2/B2_Câu1.py
+++ Bài_TH2/B2_Câu1.py
@@ -1,1 +1,10 @@
+print ( "SV: Luu Duc Loc , msv: 205751030110045 ")
+print (" ###############")
+#################
+n1= int (input (" enter n1 value"))
+n2= int (input (" enter n2 value"))
+if n1>n2 :
+   print(" n1 is big ")
+else:
+   print(" n2 is big")
 
